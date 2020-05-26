0ea3ef721d6b3b732a808f83ba48379f7d31ec91
Sumit <sumitgupta28@gmail.com>
2020-05-25 23:37:35 -0500

    init commit

---
+++ aws-sample-application/src/test-integ/java/RandomIntegTest.java
@@ -0,0 +1,47 @@
+import hello.Application;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomIntegTest {
+    private String url;
+    private ConfigurableApplicationContext springContext;
+
+    @Before
+    public void setUp() {
+        // re-set the context
+        springContext = null;
+
+        String envVariable = System.getenv("BOOT_APP_URL");
+        if (envVariable != null) {
+            // the server has been started for us - nothing to do
+            url = envVariable;
+        } else {
+            // start the server
+            springContext = Application.run();
+            url = "http://localhost:8080";
+        }
+    }
+
+    @After
+    public void tearDown() {
+        if (springContext != null) {
+            springContext.close();
+        }
+    }
+
+    @Test
+    public void example_integ_test() {
+        String response = new RestTemplate()
+                .exchange(url, HttpMethod.GET, new HttpEntity<>((String) null), String.class)
+                .getBody();
+
+        assertThat(response).contains("Greetings from the AWS CDK");
+    }
+}
